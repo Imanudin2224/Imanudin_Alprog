1bd81380e544282172704aef333028d64090f3ca
Imanudin2224 <imanudin220504@gmail.com>
2025-11-28 15:18:17 +0800

    Add files via upload

---
+++ meet5.py
@@ -0,0 +1,60 @@
+# operasi panjanglahan +
+a = 13
+b = 3
+hasil = a + b 
+print(a, "+", b, "=", hasil)
+
+# pengurangan -
+hasil = a - b
+print(a, "-", b, "=", hasil)
+
+# perkalian *
+hasil = a * b
+print(a, "*", b, "=", hasil)
+
+# pembagian /
+hasil = a / b
+print(a, "/", b, "=", hasil)
+
+# pangkat **
+hasil = a ^ b
+print(a, "^", b, "=", hasil)
+
+# modulus %
+hasil = a % b
+print(a, "mod", b, "=", hasil)
+
+# floor division //
+hasil = a // b
+print(a, "floor", b, "=", hasil)
+
+# buat operasi aritmatika yang mengabungkan 
+# semua operasi di atas 
+
+# operasi prioritas
+a = 13
+b = 3
+hasil = a + b
+print(a, "+", b, "=", hasil)
+
+hasil = a // b
+print(a, "()", b, "=", hasil)
+
+hasil = a ^ b
+print(a, "^", b, "=", hasil)
+
+hasil = a % b
+print(a, "mod", b, "=", hasil)
+
+hasil = a * b
+print(a, "*", b, "=", hasil)
+
+hasil = a - b
+print(a, "-", b, "=", hasil)
+
+hasil = a = b
+print("hasil semuannya", "=", hasil)
+# 1. ()
+# 2. pangkat akar
+# 3. : x
+# 4. + -
---
+++ pert4.py
@@ -0,0 +1,25 @@
+# mengambil inputan dari user
+
+nama = input("masukkan nama anda :")
+print("selamat datang", nama)
+print(type(nama))
+
+umur = int(input("masukan umur anda : "))
+print("umur anda sekarang : ", umur)
+print (type(umur))
+
+TinggiBadan = float (input("masukan TinggiBadan anda :"))
+print("TinggiBadan anda sekarang : ", TinggiBadan)
+print (type(TinggiBadan))
+
+hidup = bool (input("masukan hidup anda :"))
+print("umur hidup sekarang : ", hidup)
+print (type(hidup))
+
+# buatlah program dengan output sebagai berikut
+# masukkan nama anda :
+# masukkan nim anda :
+# nama anda adalah (nama anda masing2)
+# tipe data dari nama anda adalah (tipe data dari nama)
+# nim anda adalah (nim anda masing2)
+# tipe data dari nim anda adalah (tipe data dari nim)
