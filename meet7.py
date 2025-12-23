bd9b4f0bcdc0171d97a52525101e59ce15aadfe7
Imanudin2224 <imanudin220504@gmail.com>
2025-12-23 17:42:18 +0800

    Add files via upload

---
+++ meet7.py
@@ -0,0 +1,27 @@
+# operasi logika atau bolean
+# not, or, end, xor
+
+print ("========== NOT ==========")
+a = True
+b = not
+print ("nilai dari b = ", b)
+
+
+print ("========== OR(atau) ==========")
+print ("hasil akan true, selama ada satu aja true")
+a = False
+b = False
+hasil = a or b
+print (a, "OR", b, "=", hasil)
+a = False
+b = True
+hasil = a or b
+print (a, "OR", b, "=", hasil)
+a = True
+b = False
+hasil =a or b 
+print (a, "OR", b, "=", hasil)
+a = True
+b = False
+hasil =a or b 
+print (a, "OR", b, "=", hasil)
